91a505a2210e891cb353eb0d61378bb8b49132c8
jay <1311340974@qq.com>
2025-03-21 16:37:51 +0800

    lab1-v1.0

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Guts
  */
 public class LeapYear {
 
@@ -12,6 +12,15 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+    public static boolean isLeapYear(int year){
+        if(year%400==0){
+            return true;
+        }
+        if(year%4==0 && year%100!=0){
+            return true;
+        }
+        return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
